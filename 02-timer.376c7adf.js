f74b296e01752e9016918365eb1742d87833b097
HurduzOksana <112498900+HurduzOksana@users.noreply.github.com>
2022-11-20 21:43:25 +0000

    Deploying to gh-pages from @ HurduzOksana/goit-js-hw-09@b15acfdaec2ca136bd412e468ba4c3710fbd57c1 🚀

--- 02-timer.376c7adf.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=new Date(2023,5,21);document.querySelector("#datetime-picker");(function(e){setInterval((function(){new Date}),1e3)})(e)}();
-//# sourceMappingURL=02-timer.376c7adf.js.map
