b15acfdaec2ca136bd412e468ba4c3710fbd57c1
Hurduz Oksana <hurduzoksana@gmail.com>
2022-11-20 23:42:59 +0200

    Adding resolve for second task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,25 +1,96 @@
 
-// Створюємо константу - кінцеву дату
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const DEADLINE = new Date(2023, 5, 21);
+const buttonEl = document.querySelector(`[data-start]`);
+const inputEl = document.querySelector(`#datetime-picker`);
+const daysTime = document.querySelector(`[data-days]`);
+const minutesTime = document.querySelector(`[data-hours]`);
+const secondsTime = document.querySelector(`[data-minutes]`);
+const hoursTime = document.querySelector(`[data-seconds]`);
 
-// Робимо доступ до елементу
- const timerEl = document.querySelector(`#datetime-picker`)
+const thisData = new Date();
+buttonEl.disabled = true;
+let chooseSelectedDates = null;
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      if (selectedDates[0] < thisData) {
+          Notify.failure("Please choose a date in the future");
+           buttonEl.disabled = true;
+      } else {
+          buttonEl.disabled = false;
+      };
 
-// Описуємо таймер через об'єкт
+      chooseSelectedDates = selectedDates[0];
+      
+  },
+};
+
 
 const timer = {
-    // Описуємо метод запуски
+    intervalId: null,
+
+    // isActive: false,
     start(deadline) {
-        setInterval(() => {
-            const now = new Date();
-            const time = deadline - now;
-        }, 1000)
+        // if (this.isActive) {
+        //     return;
+        // }
+        // this.isActive = true;
+        this.intervalId = setInterval(() => {
+            const now = Date.now();
+            const diff = deadline - now;
+
+            if (diff <= 0) {
+                this.stop();
+                return;
+            }
+
+            const { days, hours, minutes, seconds } = this.convertMs(diff);
+
+            daysTime.textContent = this.pad(days);
+            hoursTime.textContent = this.pad(hours);
+            minutesTime.textContent = this.pad(minutes);
+            secondsTime.textContent = this.pad(seconds);
+
+        }, 1000);
     },
-    // Описуємо метод припинення
-    stop() {},
+
+    stop() {
+        clearInterval(this.intervalId);
+        // this.isActive = false;
+
+    },
+
+    pad(value) {
+        return String(value).padStart(2, 0);
+    },
+    
+    convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+    },
+
 
 };
 
-timer.start(DEADLINE);
+// timer.start(divEl,DEADLINE_DATE);
+flatpickr(inputEl, options);
+
+buttonEl.addEventListener('click', () => {
+    timer.start(chooseSelectedDates)
+});
