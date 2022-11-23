710f17c6a3c3cf5f2bd3ff3387dc2fcaf327a2ca
Hurduz Oksana <hurduzoksana@gmail.com>
2022-11-23 12:29:19 +0200

    resolve for third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,21 +1,68 @@
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// Створюємо посилання на елементи
+
+const firstDelay = document.querySelector([name = "delay"]);
+const delayStep = document.querySelector([name = "step"]);
+const amountEl = document.querySelector([name = "amount"]);
+const btnEl = document.querySelector(`button`);
+
+
+// HTML містить розмітку форми, в поля якої користувач буде вводити першу затримку в мілісекундах,
+// крок збільшення затримки для кожного промісу після першого і кількість промісів, яку необхідно створити.
+
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів,
+// скільки ввели в поле amount.Під час кожного виклику передай їй номер промісу(position), що створюється, і затримку,
+// враховуючи першу затримку(delay), введену користувачем, і крок(step).
+
+btnEl.addEventListener(`submit`, onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  // прив'язуємо amount до інпуту
+  let delay = Number(firstDelay.value);
+  let step = Number(delayStep.value);
+  let amount = Number(amountEl.value);
+  // на кожній ітерації передаєм номер промісу, затримку і крок
+  for (let i = 1; i <= amount; i += 1) { 
+    // викликаємо  проміс createPromise стільки разів скільки вказано в amount, передаємо індекс(номер) промісу і затримку
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    })
+     delay += step;
   }
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+// Функція-обробник приймає позицію та затримку
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+// Функція приймає новий проміс
+  const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (shouldResolve) {
+// Значенням промісу є об'єкт, в якому будуть властивості position і delay
+      resolve({position, delay});
+    } else {
+// Значенням промісу є об'єкт, в якому будуть властивості position і delay
+      reject({position, delay});
+    }
+    // Вказуємо затримку
+  }, delay);
   });
+  return promise;
+}
+
 
   
+  
+  // Доповни код функції createPromise таким чином, щоб вона повертала один проміс,
+  // який виконується або відхиляється через delay часу.Значенням промісу повинен бути об'єкт, 
+  // в якому будуть властивості position і delay зі значеннями однойменних параметрів.Використовуй 
+  // початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
+
+    
